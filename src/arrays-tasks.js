f2e9443433099c66cad4cbf782114e2e72f63ec3
JEvgeniy01 <evgeniy.turov@mail.ru>
2024-01-28 14:19:44 +0700

    feat: getAverage

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -161,8 +161,13 @@ function getStringsLength(arr) {
  *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
  *   getAverage([ 2, 3, 3 ])  => 2,67
  */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  if (arr.length === 0) {
+    return 0;
+  }
+  const resultSum = arr.reduce((current, sum) => current + sum, 0);
+  const average = resultSum / arr.length;
+  return Number(average.toFixed(2));
 }
 
 /**
