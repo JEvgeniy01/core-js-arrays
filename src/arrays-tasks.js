1feb391d3827435d0e03bfc726f16b9bed3e8583
JEvgeniy01 <evgeniy.turov@mail.ru>
2024-02-03 13:12:21 +0700

    feat: sortDigitNamesByNumericOrder

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -679,8 +679,37 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const obj = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9,
+    ten: 10,
+  };
+  const newArr = arr
+    .map((item) => {
+      return obj[item];
+    })
+    .sort((a, b) => a - b)
+    .map((newItem) => {
+      let resultString;
+      const keyValue = Object.entries(obj);
+      keyValue.map((pair) => {
+        if (pair[1] === newItem) {
+          [resultString] = pair;
+        }
+        return null;
+      });
+      return resultString;
+    });
+  return newArr;
 }
 
 /**
